d2cac266e8c91ddc073bd5f23a405a58fe0870aa
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-06-26 15:37:51 +0100

    clear html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,44 @@
-/* const plus = document.getElementsByTagName('i');
+const toggles = document.querySelectorAll('.toggle-icon');
 
-plus.addEventListener('click', () =>{
-    console.log('Test plus eventListener');
-}); */
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        const description = toggle.parentElement.nextElementSibling;
+        const projectBox = toggle.closest('.project-box');
+
+        // Toggle description visibility
+        description.classList.toggle('open');
+
+        // Toggle background on parent
+        projectBox.classList.toggle('open-box');
+
+        // Toggle icon
+        if (toggle.classList.contains('bx-plus')) {
+            toggle.classList.remove('bx-plus');
+            toggle.classList.add('bx-minus');
+        } else {
+            toggle.classList.remove('bx-minus');
+            toggle.classList.add('bx-plus');
+        }
+    });
+});
+
+
+//Form Script
+//to keep form from reloading
+document.getElementById('userForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', () => {
+
+    const userName = document.getElementById('name').value
+    const url = document.getElementById('url').value
+    const message = document.getElementById('message').value
+
+    console.log(`you name is: ${userName}`);
+    console.log(`your URL is: ${url}`);
+    console.log(`your message is: ${message}`)
+})
