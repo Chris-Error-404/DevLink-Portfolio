ea67b7e6ad7a3de2e19268b9ef8fac93fcc56d56
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-06-27 11:45:12 +0100

    Test message for bash commit but IntersectionObserver Script has been added

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,18 @@ window.addEventListener('scroll', () => {
 });
 
 
+
+//Fade up script
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else{
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
